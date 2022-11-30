259a536fe30cf540761af8fc00873ae98c19e1f4
Влад Булавин <vladon_2004@mail.ru>
2022-11-30 13:42:47 +0300

    found a way to parse unknown json structure

--- src/main/java/com/example/clothesservice/util/choiceTree/ChoiceTreeBuilder.java
+++ src/main/java/com/example/clothesservice/util/choiceTree/ChoiceTreeBuilder.java
@@ -1,34 +1,23 @@
 package com.example.clothesservice.util.choiceTree;
 
-import com.google.gson.JsonArray;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.Map;
 
 public class ChoiceTreeBuilder {
-    public ChoiceTree MakeTree(String dataPath){
+    private final ObjectMapper _objectMapper = new ObjectMapper();
+    public ChoiceTree MakeTree(String dataPath) throws IOException {
         var choiceTree = new ChoiceTree();
-
-        JsonArray jsonArray;
-        try {
-            var jsonString = new String(Files.readAllBytes(Path.of("C:\\main\\projects\\javaProjects\\clothes-service\\src\\main\\java\\com\\example\\clothesservice\\data.json")));
-            jsonArray = JsonParser.parseString(jsonString).getAsJsonObject().getAsJsonArray("root");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        var rootNode = new Node();
-
-        for(var el : jsonArray){
-            JsonObjectToNode(el, null);
-        }
-
+        var data = ParseJsonInputFile(dataPath);
         return new ChoiceTree();
     }
 
-    private Node JsonObjectToNode(JsonElement obj, Node rootNode){
-        var node = new Node();
-        node.set_condition(obj.getAsString());
+    private Map<String, Object> ParseJsonInputFile(String filePath) throws IOException {
+        var file = new File(filePath);
+        return _objectMapper.readValue(file, new TypeReference<>() {});
     }
 }
